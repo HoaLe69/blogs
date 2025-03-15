d31bc9b5fd6987193b8cae16feced8f27dae2b35
Try More <hoabanhanh@gmail.com>
2025-03-15 14:59:11 +0700

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+"use client"
+import { Terminal } from "lucide-react"
+import ThemeToggleButton from "./theme-toggle-button"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export default function Header() {
+  const pathname = usePathname()
+  const classLinks =
+    "tracking-wide hover:text-light-text-primary hover:dark:text-dark-text-primary dark:text-dark-text-secondary text-light-text-secondary hover:underline underline-offset-3 decoration-2"
+  const classActive = "text-light-text-primary! dark:text-dark-text-primary! underline underline-offset-3 decoration-2"
+
+  return (
+    <div className="bg-light-header-bg dark:bg-dark-header-bg backdrop-blur-md">
+      <div className="max-w-2xl mx-auto px-4">
+        <header className="flex py-2 items-center">
+          <div className="bg-logo-bg flex items-center gap-2 px-2 py-1 rounded-lg text-white">
+            <Terminal size={18} />
+            <span className="text-xl font-medium tracking-wider">LeHoa</span>
+          </div>
+          <div className="ml-auto flex items-center gap-4">
+            <ul className="flex items-center gap-3">
+              <li className={`${classLinks}  ${pathname == "/" && classActive} decoration-orange-500`}>
+                <Link href="/">Home</Link>
+              </li>
+              <li className={`${classLinks} ${pathname == "/about" && classActive} decoration-green-600`}>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+            <ThemeToggleButton />
+          </div>
+        </header>
+      </div>
+    </div>
+  )
+}
